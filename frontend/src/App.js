7e1dec55d20dbda2a0db28eb19773fcb9ffcf02e
Michael Li <michael.li@radianhub.com>
2024-07-23 14:06:17 -0700

    Add json files

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Visuals from './Components/Visuals/Visuals';
+import './App.css';
+
+function App() {
+  const [selectedVisual, setSelectedVisual] = useState('basic');
+
+  return (
+    <div className="App">
+      <Sidebar setSelectedVisual={setSelectedVisual} />
+      <div className="main-view">
+        <Visuals visual={selectedVisual} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
