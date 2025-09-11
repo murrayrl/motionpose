680921b7bc87e2ea78f3f297fd26d12067863f8e
SagiAshkenov <164096088+SagiAshkenov@users.noreply.github.com>
2025-09-11 15:41:35 -0400

    Update left_shoulder_tracker_websockets.py

--- left_shoulder_tracker_websockets.py
+++ left_shoulder_tracker_websockets.py
@@ -9,6 +9,15 @@ import threading
 import cv2
 import numpy as np
 import hailo
+import websockets
+import time
+
+# Set up the OSC client
+ip = "127.0.0.1"  # The IP address of the computer running Isadora
+port = 1234      # Isadora default port
+client = udp_client.SimpleUDPClient(ip, port)
+address_x = "/isadora-multi/1"
+address_y = "/isadora-multi/2"
 
 from hailo_apps_infra.hailo_rpi_common import (
     get_caps_from_pad,
@@ -67,19 +76,66 @@ class user_app_callback_class(app_callback_class):
     def set_frame(self, frame):      self.frame = frame
     def set_detections(self, dets):  self.detections = dets
 
+# ────────────────────────────────
+#  Websockets
+# ────────────────────────────────
+async def send_coordinates(data):
+    uri = "ws://localhost:8765"
+    try:
+        async with websockets.connect(uri) as websocket:
+            await websocket.send(json.dumps(data))
+            print("Data sent successfully")
+    except Exception as e:
+        print("data: ", data)
+        print("Failed to send data:", e) 
 
 if __name__ == "__main__":
-    Gst.init(None)
+    # commented non isadora implementation while working 
+    #Gst.init(None)
 
-    user_data = user_app_callback_class()
-    app       = GStreamerPoseEstimationApp(app_callback, user_data)
+    #user_data = user_app_callback_class()
+    #app       = GStreamerPoseEstimationApp(app_callback, user_data)
 
     # Run in background
-    threading.Thread(target=app.run, daemon=True).start()
+    #threading.Thread(target=app.run, daemon=True).start()
 
-    print("Tracking left shoulder... press Ctrl+C to stop.")
-    try:
-        while True:
-            pass  # callback handles printing
-    except KeyboardInterrupt:
-        print("\nExiting.")
+    #print("Tracking left shoulder... press Ctrl+C to stop.")
+    #try:
+    #    while True:
+    #        pass  # callback handles printing
+    #except KeyboardInterrupt:
+    #    print("\nExiting.")
+
+    # ────────────────────────────────
+    # Isadora data structure
+    # need a coordinates_data[] array
+    # array stores person_data{} dictionaries
+    # person_data has following format:
+    #     person_data = {'person': i+1,
+    #                    'keypoints': []}
+    # ex: person_data = {'person': 0 + 1, 
+    #                    'keypoints': [keypoints_dict] }
+    # keypoint_dict has following structure:
+    #     {'label': keypoint_name,
+    #     'x': float(x),
+    #     'y': float(y),
+    #     'confidence': conf}
+    # Next, only the needed keypoints, contained in kpt_list
+    # are added to list_x and list_y
+    #
+    # need list_x, list_y arrays
+    # list_x has following structure:
+    # 
+    # send all three to isadora
+    # ────────────────────────────────
+    #                                if (keypoint_names[j] in kpt_list):
+    #                                   list_x[keypoint_names[j]+"/p" + str(i+1)]= float(x)
+    #                                   list_y[keypoint_names[j] + "/p" + str(i+1)] = float(y)
+    #               
+    #                    if coordinates_data:
+    #                        await send_coordinates(coordinates_data)
+    #                        send_osc(list_x, list_y)
+
+
+    cap.release()
+    cv2.destroyAllWindows()
